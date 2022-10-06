99fb897f8d0cd4df4f64dac76e33dea1151e3d4b
Titusz Pan <titusz.pan@gmail.com>
2022-10-06 13:13:27 +0200

    Boot message: use print instead of logging

--- iscc_did_driver/main.py
+++ iscc_did_driver/main.py
@@ -7,7 +7,7 @@ import pathlib
 from blacksheep import Application
 from iscc_did_driver.options import opts
 from iscc_did_driver import __version__
-from loguru import logger as log
+
 
 HERE = pathlib.Path(__file__).parent.absolute().as_posix()
 
@@ -18,21 +18,21 @@ app.use_cors(allow_methods="*", allow_origins="*", allow_headers="*")
 
 @app.on_start
 async def boot(application: Application) -> None:
-    log.info(f" Booting iscc:did driver v{__version__}")
-    log.info(" ------------------------------------")
-    log.info(" ######    ######    ######    ###### ")
-    log.info("   ##     ##        ###       ###     ")
-    log.info("   ##      #####    ##        ##  ")
-    log.info("   ##          ##   ###       ###   ")
-    log.info(" ######   ######     ######    ######")
-    log.info(" ------------------------------------")
-    log.info(" International Standard Content Code ")
+    print(f" Booting iscc:did driver v{__version__}")
+    print(" ------------------------------------")
+    print(" ######    ######    ######    ###### ")
+    print("   ##     ##        ###       ###     ")
+    print("   ##      #####    ##        ##  ")
+    print("   ##          ##   ###       ###   ")
+    print(" ######   ######     ######    ######")
+    print(" ------------------------------------")
+    print(" International Standard Content Code ")
 
 
 @app.after_start
 async def done(application: Application) -> None:
-    log.info(" ------------------------------------")
-    log.info(" did:iscc method driver initialized! ")
+    print(" ------------------------------------")
+    print(" did:iscc method driver initialized! ")
 
 
 def main():
