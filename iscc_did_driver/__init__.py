299e1105f510dff1a0bc3df131d8e7ad2082c6c1
Titusz Pan <titusz.pan@gmail.com>
2022-10-06 12:16:24 +0200

    Add boot message with version information

--- iscc_did_driver/__init__.py
+++ iscc_did_driver/__init__.py
@@ -1,1 +1,4 @@
+import importlib.metadata
 from iscc_did_driver import controlers
+
+__version__ = importlib.metadata.version(__package__ or __name__)
--- iscc_did_driver/main.py
+++ iscc_did_driver/main.py
@@ -6,12 +6,33 @@ Implementation of https://w3c-ccg.github.io/did-resolution/ for did:iscc
 import pathlib
 from blacksheep import Application
 from iscc_did_driver.options import opts
+from iscc_did_driver import __version__
+from loguru import logger as log
 
 HERE = pathlib.Path(__file__).parent.absolute().as_posix()
 
 app = Application(show_error_details=opts.debug, debug=opts.debug)
 app.serve_files(HERE, index_document="index.html", extensions={".yaml", ".html", ".svg"})
 app.use_cors(allow_methods="*", allow_origins="*", allow_headers="*")
+
+
+@app.on_start
+async def boot(application: Application) -> None:
+    log.info(f" Booting iscc:did driver v{__version__}")
+    log.info(" ------------------------------------")
+    log.info(" ######    ######    ######    ###### ")
+    log.info("   ##     ##        ###       ###     ")
+    log.info("   ##      #####    ##        ##  ")
+    log.info("   ##          ##   ###       ###   ")
+    log.info(" ######   ######     ######    ######")
+    log.info(" ------------------------------------")
+    log.info(" International Standard Content Code ")
+
+
+@app.after_start
+async def done(application: Application) -> None:
+    log.info(" ------------------------------------")
+    log.info(" did:iscc method driver initialized! ")
 
 
 def main():
